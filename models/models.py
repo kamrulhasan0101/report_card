2fc03129723a682018bfb01902905869ca0eac45
kamrulhasan0101 <git.kamrulhasan0101@gmail.com>
2020-11-20 01:34:20 +0600

    get data from another model using function has done

--- models/models.py
+++ models/models.py
@@ -1,18 +1,37 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import api, models
 
 
-# class report_card(models.Model):
-#     _name = 'report_card.report_card'
-#     _description = 'report_card.report_card'
+class ParticularReport(models.AbstractModel):
+    _name = 'report.report_card.dis_report'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        print(docids[0])
+        docs = self.env['exam.result'].browse(docids[0])
+        add_result = self.env['additional.exam.result'].search([('student_id',
+                                                                 '=', 4)])
+        if add_result is None:
+            vals = {
+                'a_exam': "test_exam",
+                'subject': "test_sub",
+                'marks': "test_mark",
+                'roll': "test_roll"
+            }
+
+        else:
+            for a_r in add_result:
+                vals = {
+                    'a_exam': a_r.a_exam_id,
+                    'marks': a_r.obtain_marks,
+                    'roll': a_r.roll_no_id
+                }
+            additional_result = []
+        additional_result.append(vals)
+        return {
+            'doc_model': 'exam.result',
+            'data': data,
+            'docs': docs,
+            'additional_result': additional_result,
+        }
