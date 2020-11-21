69d048303ac8e01bbe8de98aa6f84f7d85f9723a
kamrulhasan0101 <git.kamrulhasan0101@gmail.com>
2020-11-21 14:15:47 +0600

    external data is pulled using function done

--- models/models.py
+++ models/models.py
@@ -11,27 +11,10 @@ class ParticularReport(models.AbstractModel):
         print(docids[0])
         docs = self.env['exam.result'].browse(docids[0])
         add_result = self.env['additional.exam.result'].search([('student_id',
-                                                                 '=', 4)])
-        if add_result is None:
-            vals = {
-                'a_exam': "test_exam",
-                'subject': "test_sub",
-                'marks': "test_mark",
-                'roll': "test_roll"
-            }
-
-        else:
-            for a_r in add_result:
-                vals = {
-                    'a_exam': a_r.a_exam_id,
-                    'marks': a_r.obtain_marks,
-                    'roll': a_r.roll_no_id
-                }
-            additional_result = []
-        additional_result.append(vals)
+                                                                 '=', docids[0])])
         return {
             'doc_model': 'exam.result',
             'data': data,
             'docs': docs,
-            'additional_result': additional_result,
+            'additional_result': add_result,
         }
